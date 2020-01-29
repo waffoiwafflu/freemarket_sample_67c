59c505478c7d3c58c7f96e51fd78a12bcf107a3c
waffoiwafflu <takoman19@gmail.comm>
2020-01-29 14:58:33 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   root to: 'post#index'
-=======
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 707aec76440d9718f3adb175c5d65f945e09f094
+
 end
