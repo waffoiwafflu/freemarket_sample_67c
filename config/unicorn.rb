f8c5b943afa7e8c882fd335fffd5f257fa636da1
hashimo1013 <hashimoto.hazimeta1013@gmail.com>
2020-01-30 11:48:45 +0900

    update-config-unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,11 +7,11 @@ worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-
 #ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
