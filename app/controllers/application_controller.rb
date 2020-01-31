16444ab9d5f263d2d5c0bb5f42364c7285228754
hashimo1013 <hashimoto.hazimeta1013@gmail.com>
2020-01-31 17:15:36 +0900

    update-AppContro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  protect_from_forgery with: :exception
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
+      # "credentialファイルを通すと上手くいかないのでとりあえず、そのまま環境変数を記載する"
+      # username == Rails.application.credentials[:basic_auth_user] && password == Rails.application.credentials[:basic_auth_password]
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-      username == 'admin1111' && password == '1111'
     end
   end
 end
