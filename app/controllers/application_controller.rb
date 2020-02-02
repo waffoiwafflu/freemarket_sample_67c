fb0371e1a52838a22d770c491599c52463c128df
hashimo1013 <hashimoto.hazimeta1013@gmail.com>
2020-02-02 16:35:50 +0900

    update_credential

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   private
 
+  def production?
+    Rails.env.production?
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      # "credentialファイルを通すと上手くいかないのでとりあえず、そのまま環境変数を記載する"
-      # username == Rails.application.credentials[:basic_auth_user] && password == Rails.application.credentials[:basic_auth_password]
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == Rails.application.credentials[:basic_auth_user] && password == Rails.application.credentials[:basic_auth_password]
     end
   end
 end
