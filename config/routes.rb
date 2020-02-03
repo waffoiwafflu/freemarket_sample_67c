0f519f33bcd7e0c0e40b72ad4f15b83975df2050
waffoiwafflu <takoman19@gmail.comm>
2020-02-03 15:43:59 +0900

    item => post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  get 'items/index'
-  root "items#index"
+  get 'posts/index'
+  root "posts#index"
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
