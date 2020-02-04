5daa915fc4be326bd804a0af9f0ada8abcaab31d
waffoiwafflu <takoman19@gmail.comm>
2020-02-04 15:47:20 +0900

    test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,38 @@
 class ItemsController < ApplicationController
   
   def index
-  
-  end
-  
-  def show
-    @item = Item.new
-    render "new"
+    @items = Item.includes(:images).order('created_at DESC')
   end
 
   def new
     @item = Item.new
+    @item.images.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, images_attributes: [:src])
+  end
+  
 end
+
