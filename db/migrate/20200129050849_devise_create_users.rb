3c5a58ab12763bd24e50509a9799a9b72cc99107
morimoripcpc <58966192+morimoripcpc@users.noreply.github.com>
2020-02-05 10:35:09 +0900

    ertyuiop@

--- db/migrate/20200129050849_devise_create_users.rb
+++ db/migrate/20200129050849_devise_create_users.rb
@@ -47,6 +47,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+
+
+      # t.integer  :sign_in_count, default: 0, null: false.
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
