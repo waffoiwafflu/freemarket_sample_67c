5d760e380724ba0e6d163310277d3f85bd654105
morimoripcpc <58966192+morimoripcpc@users.noreply.github.com>
2020-02-05 16:31:54 +0900

    えrdtfg

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   
+ 
   get 'posts/index'
   root "posts#index"
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  # post do
+  #   user
+  # end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
