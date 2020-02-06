912b1f4b88693705c05fba36ff9cfa5e9019bd36
waffoiwafflu <takoman19@gmail.comm>
2020-02-06 17:19:05 +0900

    aaa

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   has_many :images, dependent: :destroy
+  validates :images, presence: true
 end
