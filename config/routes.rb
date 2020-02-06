88d08d8c9f434d8832174a16fe05c2ffa5a858a2
morimoripcpc <58966192+morimoripcpc@users.noreply.github.com>
2020-02-06 19:03:26 +0900

    dfghjkl;

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
     get '/users/sign_out' => 'devise/sessions#destroy'
+    # get '/users' => 'devise/registrations#create'
   end
 
 
