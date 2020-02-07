f1a2fa622498b733a60ccb8d2b6a6eda3ae74b2c
waffoiwafflu <takoman19@gmail.comm>
2020-02-07 17:07:41 +0900

    testmode

---
+++ spec/factories/item.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+
+
+  factory :item do
+    name                  {"test"}
+    detail                {"test"}
+    category              {"1"}
+    price                 {"1000"}
+    status                {"1"}
+    state                 {"1"}
+    city                  {"1"}
+    delivery              {"1"}
+    delivery_time         {"1"}
+    fee_payer             {"1"}
+    images {[
+      FactoryBot.build(:image, item: nil)  
+    ]}
+   
+    
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,3 +54,85 @@ require 'rails_helper'
     
 
 # end
+describe Item do
+    describe '#create' do
+      it "nameが空では登録不可" do
+        item = build(:item, name: "aaa")
+        item.valid?
+        expect(product.errors[:name]).to include("を入力してください")
+      end
+  
+      it "nameが不適切ワードでは登録不可" do
+        product = build(:product, name: "aaa")
+        product.valid?
+        expect(product.errors[:name]).to include("に不適切な単語が含まれています")
+      end
+  
+      it "detailが空では登録不可" do
+        product = build(:product, detail: "")
+        product.valid?
+        expect(product.errors[:detail]).to include("を入力してください")
+      end
+  
+      it "detailが不適切ワードでは登録不可" do
+        product = build(:product, detail: "アホ")
+        product.valid?
+        expect(product.errors[:detail]).to include("に不適切な単語が含まれています")
+      end
+  
+      it "categoryが空では登録不可" do
+        product = build(:product, category: "")
+        product.valid?
+        expect(product.errors[:category]).to include("を入力してください")
+      end
+  
+      it "priceが空では登録不可" do
+        product = build(:product, price: "")
+        product.valid?
+        expect(product.errors[:price]).to include("を入力してください")
+      end
+  
+      it "statusが空では登録不可" do
+        product = build(:product, status: "")
+        product.valid?
+        expect(product.errors[:status]).to include("を入力してください")
+      end
+  
+      it "stateが空では登録不可" do
+        product = build(:product, state: "")
+        product.valid?
+        expect(product.errors[:state]).to include("を入力してください")
+      end
+  
+      it "cityが空では登録不可" do
+        product = build(:product, city: "")
+        product.valid?
+        expect(product.errors[:city]).to include("を入力してください")
+      end
+  
+      it "deliveryが空では登録不可" do
+        product = build(:product, delivery: "")
+        product.valid?
+        expect(product.errors[:delivery]).to include("を入力してください")
+      end
+      
+      it "delivery_timeが空では登録不可" do
+        product = build(:product, delivery_time: "")
+        product.valid?
+        expect(product.errors[:delivery_time]).to include("を入力してください")
+      end
+  
+      it "fee_payerが空では登録不可" do
+        product = build(:product, fee_payer: "")
+        product.valid?
+        expect(product.errors[:fee_payer]).to include("を入力してください")
+      end
+  
+      it "すべて満たしていれば登録可" do
+        product = create(:product)
+        expect(product).to be_valid
+      end
+  
+  
+    end
+  end
