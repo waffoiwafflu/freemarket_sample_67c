7262b28cfeeb217e6950332747ad951c8284aeb9
koheiy1 <59187820+koheiy1@users.noreply.github.com>
2020-02-12 14:43:47 +0900

    com

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_item, only: [:done]
   def index
     @parents = Category.all.order("id ASC").limit(13)
     @items = Item.all.includes(:images).order("created_at desc").limit(3)
@@ -16,4 +17,11 @@ class PostsController < ApplicationController
     @item_buyer = Item.find(params[id])
     @item_buyer.updete( buyer_id: current_user.id)
   end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
 end
