3441b8c96f221307e28cbe28986ffc49ae67f241
hashimo1013 <hashimoto.hazimeta1013@gmail.com>
2020-02-20 19:26:06 +0900

    cmcmc

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,29 +1,32 @@
 class LikesController < ApplicationController
-  before_action :set_item, only: [:create, :destory]
-  def create
+  before_action :set_item, only: [:create, :destroy]
+  before_action :set_like, only: [:create, :destroy]
 
+  def create
     if @like = Like.create(user_id: current_user.id, item_id: params[:item_id])
       flash[:alert] = "いいねしました"
     else
       flash.now[:alert] = "いいねできませんでした"
     end
-    @like.reload
-    @likes = Like.where(item_id: params[:item_id])
-    # set_item
+    @item.reload
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
     if like.destroy
-      flash[:alert] = "いいね削除しました"
+      flash.now[:alert] = "いいね削除しました"
     else
       flash.now[:alert] = 'いいね削除できませんでした。'
     end
-    @likes = Like.where(item_id: params[:item_id])
-    # set_item
+    @item.reload
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def set_like
+    @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -88,8 +88,12 @@ ActiveRecord::Schema.define(version: 2020_02_17_032224) do
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -99,7 +103,7 @@ ActiveRecord::Schema.define(version: 2020_02_17_032224) do
     t.string "last_name_kana", null: false
     t.datetime "year", null: false
     t.string "phone_number", null: false
-    t.integer "nickname", null: false
+    t.string "nickname", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.string "reset_password_token"
@@ -111,4 +115,5 @@ ActiveRecord::Schema.define(version: 2020_02_17_032224) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sns_credentials", "users"
 end
